04bedb34a04041c2e41c8fca1c658b4829611ae1
Eric Burns <eburns@nvisia.com>
2017-04-28 16:10:01 -0500

    Updates to python src files

--- bot_runner_wizard.py
+++ bot_runner_wizard.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from slack_taco_bot import SlackTacoBot
 
--- src/slack_taco_bot.py
+++ src/slack_taco_bot.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 '''
 Created on Apr 28, 2017
 
